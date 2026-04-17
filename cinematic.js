deca377f1bc702b8a0e61f42b6f85a7e9884e822
jamnout <topadam7@gmail.com>
2026-04-17 15:46:14 -0700

    Peer Coding Team (Names in README)

--- cinematic.js
+++ cinematic.js
@@ -1,5 +1,37 @@
+class AdamGame extends Phaser.Scene {
+    constructor() {
+        super('adamgame');
+    }
+    preload(){
+
+    }
+    create(){
+        this.graphics = this.add.graphics();
+
+        this.graphics.fillStyle(0xb0711c, 1);
+       // this.graphics.fillRect(400, 350, 300, 150)
+        this.graphics.fillEllipse(500, 400, 300, 200, 30);
+        this.graphics.fillEllipse(700, 500, 250, 200);
+
+        this.graphics.fillStyle(0xE68809, .5);
+        this.graphics.fillEllipse(400, 300, 250, 200, 30);
+
+        this.graphics.fillStyle(0xE3C9A3, .8);
+        this.graphics.fillTriangle(200, 300, 330, 200, 315, 340);
 
 
-const game = new Phaser.game({
+    }
+    update(){
 
-})
+    }
+}
+
+let config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: 0xffffff,
+    scene: [AdamGame],
+}
+
+let game = new Phaser.Game(config);
